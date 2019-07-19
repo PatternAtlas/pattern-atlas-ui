3c24a3d8f5822eccd5b84f62c0868bdb6bb03b63
Manuela Weigold <st159210@stud.uni-stuttgart.de>
2019-07-19 13:28:10 +0200

    don't allow the user to edit the section headings
    (cherry picked from commit 5d1814f)

--- src/app/pattern-language-management/create-pattern/create-pattern.component.ts
+++ src/app/pattern-language-management/create-pattern/create-pattern.component.ts
@@ -7,7 +7,7 @@ import { IriConverter } from '../../core/util/iri-converter';
 import { Logo } from '../../core/service/data/Logo.interface';
 import { GithubPersistenceService } from '../../core/service/github-persistence.service';
 import * as marked from 'marked';
-import { TokensList } from 'marked';
+import { Token, TokensList } from 'marked';
 import Pattern from '../../core/model/pattern.model';
 import { PatternOntologyService } from '../../core/service/pattern-ontology.service';
 import { SectionResponse } from '../../core/service/data/SectionResponse.interface';
@@ -15,6 +15,7 @@ import { ToasterService } from 'angular2-toaster';
 import { PlRestrictionLoaderService } from '../../core/service/loader/pattern-language-loader/pl-restriction-loader.service';
 import { PatternLanguageSectionRestriction } from '../../core/model/PatternLanguageSectionRestriction.model';
 import { switchMap } from 'rxjs/internal/operators';
+import PatternPedia from '../../core/model/pattern-pedia.model';
 
 
 @Component({
@@ -31,10 +32,22 @@ export class CreatePatternComponent implements OnInit {
   sections: string[];
   plLogos: string[];
   plRestrictions: Map<string, PatternLanguageSectionRestriction[]>;
+  xsdPrefix = new PatternPedia().defaultPrefixes.get('xsd').replace('<', '').replace('>', '');
 
+  defaultTextForType: Map<string, string> =
+    new Map([
+      ['http://purl.org/dc/dcmitype/Image', '![](http://)'],
+      [this.xsdPrefix + 'anyURI', '<Insert/your/URI>'],
+      [this.xsdPrefix + 'integer', 'Replace this line by an Integer.'],
+      [this.xsdPrefix + 'positiveInteger', 'Replace this line by a positive Integer.'],
+      [this.xsdPrefix + 'nonNegativeInteger', 'Replace this line by a positive Integer.'],
+      [this.xsdPrefix + 'nonPositiveInteger', 'Replace this line by a negative Integer.'],
+      [this.xsdPrefix + 'negativeInteger', 'Replace this line by a negative Integer.'],
+      [this.xsdPrefix + 'date', 'dd/MM/yyyy'],
+    ]);
 
   @ViewChild('textEditor') private _textEditor: TdTextEditorComponent;
-  patternLanguageStructure = `# Pattern name`;
+  previousTextEditorValue = `# Pattern name`;
 
   options: any = {
     // todo: hide the preview button because it forces fullscreen mode (and destroys our page layout)
@@ -55,6 +68,7 @@ export class CreatePatternComponent implements OnInit {
   ngOnInit() {
     this.plIri = IriConverter.convertIdToIri(this.activatedRoute.snapshot.paramMap.get('plid'));
     this.loader.supportedIRI = this.plIri;
+    console.log(this.defaultTextForType);
 
     this.patternOntologyServce.loadUrisToStore([{value: this.plIri, token: null}]).then(() => {
       this.loadPatternInfos();
@@ -120,8 +134,31 @@ export class CreatePatternComponent implements OnInit {
   }
 
   onChangeMarkdownText(): void {
+    let currentText = this.parseMarkdownText();
+    if (this.invalidTextEdit(currentText)) {
+      console.log('invalid');
+      this._textEditor.value = this.previousTextEditorValue;
+      currentText = this.parseMarkdownText();
+      this.toastService.pop('warning', 'Reset text', `Title of Sections changed, this is not allowed`);
+    }
+    document.getElementById('preview').innerHTML = marked.parser(currentText);
+  }
 
-    document.getElementById('preview').innerHTML = marked.parser(this.parseMarkdownText());
+  // returns if a user changed the value of the sections headers (which he is not allowed to do)
+  private invalidTextEdit(currentText: marked.TokensList): boolean {
+    // we should find a corresponding line (= that starts with ## followed by section name) for each section
+    for (const section of this.sections) {
+      const indexOfCorrespondingLine = currentText.findIndex(sec => this.isSectionHeading(sec) &&
+        && this.ignoreCaseAndWhitespace(sec.text) === this.ignoreCaseAndWhitespace(this.addSpaceForCamelCase(this.getSectionTitle(section))));
+      if (indexOfCorrespondingLine === -1) {
+        return true;
+      }
+    }
+    // there should be only one patternname (= line that starts with # )
+    return !(currentText.filter(it => it.type === 'heading' && it.depth === 1).length === 1)
+      || // there should be as many second headings as sections (= line that starts with # )
+      !(currentText.filter(it => it.type === 'heading' && it.depth === 2).length === this.sections.length);
+
   }
 
   private parsePatternInput(): Pattern {
@@ -130,8 +167,8 @@ export class CreatePatternComponent implements OnInit {
     const patternname = patternNameIndex !== -1 ? lines[patternNameIndex]['text'] : '';
     const sectionMap = new Map<string, string | string[]>();
     this.sections.forEach((section: string) => {
-      const sectionIndex = lines.findIndex((it) => it.type === 'heading' && it.depth === 2 &&
-        this.ignoreCaseAndWhitespace(it.text) === this.ignoreCaseAndWhitespace(this.addSpaceForCamelCase(this.getSectionTitle(section))));
+      const sectionIndex = lines.findIndex((sec) => this.isSectionHeading(sec) &&
+        this.ignoreCaseAndWhitespace(sec.text) === this.ignoreCaseAndWhitespace(this.addSpaceForCamelCase(this.getSectionTitle(section))));
       if (sectionIndex !== -1) {
         let sectioncontent = '';
         for (let i = sectionIndex + 1; i < lines.length; i++) {
@@ -157,9 +194,13 @@ export class CreatePatternComponent implements OnInit {
     return text.replace(/([a-z])([A-Z])/g, '$1 $2');
   }
 
+  private isSectionHeading(line: Token): boolean {
+    return line.type === 'heading' && line.depth === 2;
+  }
+
 
   getDefaultTextForSection(section: string): string {
-    const prefix = 'http://www.w3.org/2001/XMLSchema#';
+    const xsdPrefix = 'http://www.w3.org/2001/XMLSchema#';
     if (!this.plRestrictions) {
       return null;
     }
@@ -167,16 +208,12 @@ export class CreatePatternComponent implements OnInit {
       return !!rest.type;
     });
     const sectionType = restrictionWithTypeIndex !== -1 ? this.plRestrictions.get(section)[restrictionWithTypeIndex].type : '';
-    if (sectionType === (prefix + 'positiveInteger') || (sectionType === 'xsd:positiveInteger')) {
-      return 'Enter a positive Integer.';
+    const defaultText = this.defaultTextForType.get(sectionType);
+    if (!defaultText) {
+      return 'Enter your input for this section here.';
     }
-    if (sectionType === (prefix + 'string') || (section === 'xsd:string')) {
-      return 'Enter your text for this section here.';
-    }
-    if (sectionType === (prefix + 'anyURI') || (section === 'xsd:anyURI')) {
-      return '<Enter/your/URI/or/URL>';
-    }
-    return 'Enter your input for this section here.';
+    console.log(defaultText);
+    return defaultText;
   }
 
   private loadPatternInfos() {
@@ -199,21 +236,16 @@ export class CreatePatternComponent implements OnInit {
 
   private loadRestrictionsAndInitPatternEditor() {
     this.loader.getPLSections(this.plIri).then((res: SectionResponse[]) => {
-      console.log('sections: ');
-      console.log(res);
       this.sections = res.map((iri: any) => {
         return this.reconstructSectionFromSectionesult(iri);
       });
-      console.log('parsed sections: ');
-      console.log(this.sections);
       this.PlRestrictionLoader.loadContentFromStore().then((response: any) => {
         this.plRestrictions = response;
-        console.log(this.plRestrictions);
         for (const section of this.sections) {
-          this.patternLanguageStructure = this.patternLanguageStructure.concat(
+          this.previousTextEditorValue = this.previousTextEditorValue.concat(
             '\n ## ' + this.addSpaceForCamelCase(this.getSectionTitle(section)) + '\n' + this.getDefaultTextForSection(section));
         }
-        this._textEditor.value = this.patternLanguageStructure;
+        this._textEditor.value = this.previousTextEditorValue;
         this.onChangeMarkdownText();
       });
 
