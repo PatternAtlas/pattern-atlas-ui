3369cacc301bacfee9164ca8026d414f743686e6
Manuela Weigold <st159210@stud.uni-stuttgart.de>
2019-07-21 15:31:28 +0200

    commit missing files
    (cherry picked from commit 4a3f59f)

---
+++ src/app/core/component/type-templates/xsd/date/date.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { DialogData, MdEditorComponent } from '../../../md-editor/md-editor.component';
+import { MatDialog } from '@angular/material';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'pp-date',
+  templateUrl: './date.component.html',
+  styleUrls: ['./date.component.scss']
+})
+export class DateComponent implements OnInit {
+
+  data: any;
+  date = new FormControl(new Date());
+
+  showActionButtons = false;
+
+  constructor(private dialog: MatDialog) {
+  }
+
+  ngOnInit() {
+  }
+
+  openEditor(): void {
+    const dialogRef = this.dialog.open(MdEditorComponent,
+      {data: {content: this.data}});
+    dialogRef.afterClosed().subscribe(async (result: DialogData) => {
+    });
+  }
+}
