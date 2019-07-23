6242fab7ce4284c0a2cbe8392eeaa9711a2c3c27
Manuela Weigold <st159210@stud.uni-stuttgart.de>
2019-07-23 14:30:21 +0200

    don't load filecontents synchronously

--- src/app/core/service/pattern-ontology.service.ts
+++ src/app/core/service/pattern-ontology.service.ts
@@ -280,9 +280,10 @@ export class PatternOntologyService implements SparqlExecutor {
   // Given an array of iris retrieved by a SPARQL query (QueriedData), load the content into store
   async loadQueriedIrisToStore(patternGraphList: QueriedData[]) {
     const urisToLoad = IriConverter.extractDataValue(patternGraphList);
-    for (let i = 0; i < urisToLoad.length; i++) {
-      await this.loadUriToStore(urisToLoad[i]);
-    }
+
+    const promises = urisToLoad.map(uri => this.loadUriToStore(uri));
+    await Promise.all(promises);
+
     console.log('LOADED Uri Dependencies!');
     }
 
