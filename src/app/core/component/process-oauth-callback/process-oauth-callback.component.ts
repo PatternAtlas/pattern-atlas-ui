c3856cd673902d96e36d9690e2c38b9013369039
Manuela Weigold <st159210@stud.uni-stuttgart.de>
2019-08-14 18:37:54 +0200

    refactoring: remove unused code

--- src/app/core/component/process-oauth-callback/process-oauth-callback.component.ts
+++ src/app/core/component/process-oauth-callback/process-oauth-callback.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
-import { GithubPersistenceService } from '../../service/github-persistence.service';
 import { switchMap } from 'rxjs/internal/operators';
 
 @Component({
@@ -12,14 +11,12 @@ import { switchMap } from 'rxjs/internal/operators';
 })
 export class ProcessOauthCallbackComponent implements OnInit {
 
-  constructor(private _httpClient: HttpClient, private _route: ActivatedRoute, private _cookieService: CookieService, private _router: Router,
-              private _githubPersistenceService: GithubPersistenceService) {
+  constructor(private _httpClient: HttpClient, private _route: ActivatedRoute, private _cookieService: CookieService, private _router: Router) {
     this._route.params.subscribe( params => console.log(params) );
 
   }
 
   ngOnInit() {
-    const code = this._route.snapshot.params['code'];
     this._route.queryParams.pipe(
       switchMap((params) => {
         return this._httpClient.get('https://eqjjnlkv6a.execute-api.eu-central-1.amazonaws.com/default/authenticate/' + params['code']);
