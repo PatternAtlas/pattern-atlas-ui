1a980063dcb1c116c6a36323b9a6e179094a40ac
Manuela Weigold <st159210@stud.uni-stuttgart.de>
2019-08-15 14:56:57 +0200

    parse oauthtoken response

--- src/app/core/component/process-oauth-callback/process-oauth-callback.component.ts
+++ src/app/core/component/process-oauth-callback/process-oauth-callback.component.ts
@@ -19,13 +19,17 @@ export class ProcessOauthCallbackComponent implements OnInit {
   ngOnInit() {
     this._route.queryParams.pipe(
       switchMap((params) => {
-        return this._httpClient.get('https://eqjjnlkv6a.execute-api.eu-central-1.amazonaws.com/default/authenticate/' + params['code']);
+        return this._httpClient.get('https://eqjjnlkv6a.execute-api.eu-central-1.amazonaws.com/default/authenticate/' + params['code'], {responseType: 'text'});
       })).subscribe(
       (res) => {
+        const response = res.replace('"', '');
         // github sends the access_token in url param style (access_token=...&...), so let's use this info to decode the token:
-      const urlResponse = new URLSearchParams(res);
-      this._cookieService.set('patternpedia_github_token', urlResponse.get('access_token'));
-      this._router.navigate(['..'], {relativeTo: this._route});
+        const urlResponse = new URLSearchParams(response);
+        if (urlResponse.get('access_token')) {
+          this._cookieService.set('patternpedia_github_token', urlResponse.get('access_token'));
+        }
+
+        this._router.navigate(['..'], {relativeTo: this._route});
       });
   }
 
