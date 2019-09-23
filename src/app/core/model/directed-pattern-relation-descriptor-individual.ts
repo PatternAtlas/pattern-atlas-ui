2bfad5e9356565619beca32e3efa6a2fb357f776
Manuela Weigold <st159210@stud.uni-stuttgart.de>
2019-09-23 18:07:46 +0200

    whitespace in pattern names should not be present in uris of individuals

--- src/app/core/model/directed-pattern-relation-descriptor-individual.ts
+++ src/app/core/model/directed-pattern-relation-descriptor-individual.ts
@@ -1,5 +1,6 @@
 import Pattern from './pattern.model';
 import { PatternRelationDescriptorIndividual } from './pattern-relation-descriptor-individual';
+import { IriConverter } from '../util/iri-converter';
 
 
 export class DirectedPatternRelationDescriptorIndividual extends PatternRelationDescriptorIndividual{
@@ -12,15 +13,15 @@ export class DirectedPatternRelationDescriptorIndividual extends PatternRelation
     this.source = source;
     this.target = target;
     this.description = description;
-    this.individualName = this.source.name + '-to-' + this.target.name;
+    this.individualName = IriConverter.removeWhitespace(this.source.name) + '-to-' + IriConverter.removeWhitespace(this.target.name);
   }
 
   toTurtle(): string {
     const ary = [];
     ary.push(`:${this.individualName}`);
     ary.push(`rdf:type owl:NamedIndividual , pp:DirectedPatternRelationDescriptor ;`);
-    ary.push(`pp:hasSource :${this.source.name} ; `);
-    ary.push(`pp:hasTarget :${this.target.name} ${this.description ? '; \n pp:hasDescription "' + this.description + '"^^xsd:string . ' : '.'}`);
+    ary.push(`pp:hasSource :${IriConverter.removeWhitespace(this.source.name)} ; `);
+    ary.push(`pp:hasTarget :${IriConverter.removeWhitespace(this.target.name)} ${this.description ? '; \n pp:hasDescription "' + this.description + '"^^xsd:string . ' : '.'}`);
     ary.push(' ');
     return ary.join('\n');
   }
--- src/app/core/model/undirected-pattern-relation-descriptor-individual.ts
+++ src/app/core/model/undirected-pattern-relation-descriptor-individual.ts
@@ -1,5 +1,6 @@
 import Pattern from './pattern.model';
 import { PatternRelationDescriptorIndividual } from './pattern-relation-descriptor-individual';
+import { IriConverter } from '../util/iri-converter';
 
 
 export class UndirectedPatternRelationDescriptorIndividual extends PatternRelationDescriptorIndividual{
@@ -10,15 +11,15 @@ export class UndirectedPatternRelationDescriptorIndividual extends PatternRelati
     super();
     this.hasPattern = [firstPattern, secondPattern];
     this.description = description;
-    this.individualName = firstPattern.name + '-to-' + secondPattern.name;
+    this.individualName = IriConverter.removeWhitespace(firstPattern.name) + '-to-' + IriConverter.removeWhitespace(secondPattern.name);
   }
 
   toTurtle(): string {
     const ary = [];
     ary.push(`:${this.individualName}`);
     ary.push(`rdf:type owl:NamedIndividual , pp:UndirectedPatternRelationDescriptor ;`);
-    ary.push(`pp:hasPattern :${this.hasPattern[0].name} ; `);
-    ary.push(`pp:hasPattern :${this.hasPattern[1].name} ${this.description ? '; \n pp:hasDescription "' + this.description + '"^^xsd:string . ' : '.'}`);
+    ary.push(`pp:hasPattern :${IriConverter.removeWhitespace(this.hasPattern[0].name)} ; `);
+    ary.push(`pp:hasPattern :${IriConverter.removeWhitespace(this.hasPattern[1].name)} ${this.description ? '; \n pp:hasDescription "' + this.description + '"^^xsd:string . ' : '.'}`);
     ary.push(' ');
     return ary.join('\n');
   }
