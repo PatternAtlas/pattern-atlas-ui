67cbab294809bf859d703454b8fa687ea3944b0d
Manuela Weigold <st159210@stud.uni-stuttgart.de>
2019-09-26 17:01:45 +0200

    add simplified version of the graph view as a default renderer show default renderers (card/graph) if no custom renderer was registered for the language

--- src/app/pattern-language-management/pattern-language-management.module.ts
+++ src/app/pattern-language-management/pattern-language-management.module.ts
@@ -1,19 +1,4 @@
 import { GraphModule } from './../graph/graph.module';
-import { GraphComponent } from './../graph/component/graph/graph.component';
-/*
- * Copyright (c) 2018 University of Stuttgart.
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0, or the Apache Software License 2.0
- * which is available at https://www.apache.org/licenses/LICENSE-2.0.
- *
- * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
- */
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -48,6 +33,20 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { ProcessOauthCallbackComponent } from '../core/component/process-oauth-callback/process-oauth-callback.component';
 import { ToasterModule } from 'angular2-toaster';
 import { PatternLanguageGraphComponent } from './pattern-language-graph/pattern-language-graph.component';
+
+/*
+ * Copyright (c) 2018 University of Stuttgart.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
 
 @NgModule({
     imports: [
