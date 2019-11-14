e45c0c959965d8291a9daf4e6facdbc38d865b69
manuwei <manuela.weigold@iaas.uni-stuttgart.de>
2019-11-14 16:38:14 +0100

    remove stackedit testing

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import Stackedit from 'stackedit-js';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'pp-test',
@@ -9,31 +7,7 @@ import Stackedit from 'stackedit-js';
 })
 export class TestComponent implements OnInit {
 
-    textValue: string;
-    stackedit: any;
-
-    constructor(private http: HttpClient) {
-    }
 
     ngOnInit() {
-        console.log('HELLO');
-        // this.doStuff();
-        this.stackedit = new Stackedit();
-        this.stackedit.on('fileChange', (file) => {
-            console.log(file);
-        });
-
-        this.stackedit.on('close', (file) => {
-            console.log(file);
-        });
-    }
-
-    async doStuff() {
-        this.stackedit.openFile({
-            name: 'Filename', // with a filename
-            content: {
-                text: this.textValue // and the Markdown content.
-            }
-        });
     }
 }
