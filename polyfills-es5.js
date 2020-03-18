1dd6566c56b782fa480cd09967e2fa123ec9358c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-18 07:27:53 +0000

    Deploy PatternPedia/pattern-pedia-views-ui to github.com/PatternPedia/pattern-pedia-views-ui.git:gh-pages

--- polyfills-es5.js
+++ polyfills-es5.js
@@ -15873,8 +15873,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/travis/build/PatternPedia/pattern-atlas/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
-module.exports = __webpack_require__(/*! /home/travis/build/PatternPedia/pattern-atlas/src/polyfills.ts */"./src/polyfills.ts");
+__webpack_require__(/*! /home/travis/build/PatternPedia/pattern-pedia-views-ui/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
+module.exports = __webpack_require__(/*! /home/travis/build/PatternPedia/pattern-pedia-views-ui/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
--- polyfills.js
+++ polyfills.js
@@ -5863,7 +5863,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/travis/build/PatternPedia/pattern-atlas/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/travis/build/PatternPedia/pattern-pedia-views-ui/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
