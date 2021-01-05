d8cdff39072af0e8896091429d5a8a4c4a460f88
beiselmn <st111845@stud.uni-stuttgart.de>
2021-01-05 11:10:29 +0100

    Adding edit-url-dialog

---
+++ src/app/core/component/edit-url-dialog/edit-url-dialog.component.ts
@@ -0,0 +1,19 @@
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+
+@Component({
+  selector: 'pp-edit-url-dialog',
+  templateUrl: './edit-url-dialog.component.html',
+  styleUrls: ['./edit-url-dialog.component.scss']
+})
+export class EditUrlDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<EditUrlDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data) {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
