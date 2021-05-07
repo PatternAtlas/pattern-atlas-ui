86f30b77493cf4dbf270ce65faa087182a92c4cd
Manuela Weigold <manuela.weigold@iaas.uni-stuttgart.de>
2021-05-07 13:24:37 +0200

    remove unused config settings

--- src/assets/env.js
+++ src/assets/env.js
@@ -11,7 +11,6 @@
   window['env']['LATEX_RENDERER_HOST_NAME'] = 'localhost';
   window['env']['CONFIG_SERVER_PORT'] = 2379;
   window['env']['QC_ATLAS_PORT'] = 8080;
-  window['env']['NISQ_ANALYZER_PORT'] = 8081;
   window['env']['PATTERN_ATLAS_PORT'] = 8082;
   window['env']['LATEX_RENDERER_PORT'] = 8083;
 })(this);
