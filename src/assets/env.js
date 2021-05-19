cf5f2f5bc3463ce33bda1fda5728cbeda0017704
Manuela Weigold <manuela.weigold@iaas.uni-stuttgart.de>
2021-05-19 14:15:08 +0200

    fix missing env.js template error

--- src/assets/env.js
+++ src/assets/env.js
@@ -5,12 +5,8 @@
 
   window['env']['production'] = false;
   window['env']['CONFIG_SERVER_HOST_NAME'] = 'localhost';
-  window['env']['QC_ATLAS_HOST_NAME'] = 'localhost';
-  window['env']['NISQ_ANALYZER_HOST_NAME'] = 'localhost';
-  window['env']['PATTERN_ATLAS_HOST_NAME'] = 'localhost';
   window['env']['LATEX_RENDERER_HOST_NAME'] = 'localhost';
+  window['env']['PATTERN_ATLAS_HOST_NAME'] = 'localhost';
   window['env']['CONFIG_SERVER_PORT'] = 2379;
-  window['env']['QC_ATLAS_PORT'] = 8080;
-  window['env']['PATTERN_ATLAS_PORT'] = 8082;
   window['env']['LATEX_RENDERER_PORT'] = 8083;
 })(this);
