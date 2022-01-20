48d6856db854062d05f85786a087aa3303cddc80
Marvin Bechtold <marvin.bechtold.dev@gmail.com>
2022-01-20 09:14:09 +0100

    Feature: hide empty sections of a pattern (#132)
    Signed-off-by: Marvin Bechtold <marvin.bechtold.dev@gmail.com>

--- src/app/core/component/markdown-content-container/markdown-pattern-sectioncontent/markdown-pattern-section-content.component.ts
+++ src/app/core/component/markdown-content-container/markdown-pattern-sectioncontent/markdown-pattern-section-content.component.ts
@@ -44,7 +44,8 @@ export class MarkdownPatternSectionContentComponent extends DataRenderingCompone
   isCommentingEnabled = false;
   showCommentButton = true;
   showActionButtons = false;
-  editingFromConfigServer = false
+  editingFromConfigServer = false;
+  showSection = true;
   @ViewChild('markdownContent') markdownDiv: ElementRef;
   @Input() content: string;
   private markdown: MarkdownIt;
@@ -71,6 +72,7 @@ export class MarkdownPatternSectionContentComponent extends DataRenderingCompone
   changeText(value: string): void {
     this.markdownDiv.nativeElement.innerHTML = '';
     this.renderedData = value;
+    this.showSection = this.renderedData && this.renderedData.length > 0;
     this.renderSVGTags(value);
 
     this.cdr.detectChanges();
