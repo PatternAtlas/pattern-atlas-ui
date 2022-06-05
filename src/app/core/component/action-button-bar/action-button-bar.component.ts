75ce442424ab6bf5591c155df2e8c29d5f45c9a1
Alex <alexan.mandl@gmail.com>
2022-06-05 13:20:18 +0200

    added missing privilege toggle for issues

--- src/app/core/component/action-button-bar/action-button-bar.component.ts
+++ src/app/core/component/action-button-bar/action-button-bar.component.ts
@@ -4,6 +4,8 @@ import {
 import {
   PatternAtlasUiRepositoryConfigurationService, UiFeatures
 } from 'src/app/core/directives/pattern-atlas-ui-repository-configuration.service';
+import { PrivilegeService } from '../../../authentication/_services/privilege.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'pp-action-button-bar',
@@ -23,10 +25,12 @@ export class ActionButtonBarComponent implements OnInit {
   @Input() reloadButton = false;
   @Input() goBackButton = true;
   @Input() secondAddButton: boolean;
-  @Input() firstAddButton = true;
+  @Input() firstAddButton = false;
   @Input() secondAddButtonText: string;
   @Input() iconEdit = false;
   @Input() iconUrl: string;
+  // Should the name of a privilege be given, the add button is only visible if the user has this privilege
+  @Input() addPrivilegeName: string;
 
   @Input() back = false;
   @Output() backClicked = new EventEmitter<void>();
@@ -37,11 +41,19 @@ export class ActionButtonBarComponent implements OnInit {
 
   constructor(private cdr: ChangeDetectorRef,
               private applicationRef: ApplicationRef,
-              private configurationService: PatternAtlasUiRepositoryConfigurationService) {
+              private configurationService: PatternAtlasUiRepositoryConfigurationService,
+              private p: PrivilegeService) {
   }
 
   ngOnInit() {
     this.editingFromConfigServer = this.configurationService.configuration.features[UiFeatures.EDITING];
+    if(this.addPrivilegeName) {
+      // Check if user privilege is present
+      this.p.hasPrivilege(this.addPrivilegeName)
+        .subscribe(value => this.firstAddButton = value);
+    } else {
+      this.firstAddButton = true
+    }
   }
 
   addButtonClicked() {
