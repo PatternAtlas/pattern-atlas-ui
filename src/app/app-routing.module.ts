17efaa066ba859d139137db872db03926d3c7578
Alex <alexan.mandl@gmail.com>
2022-11-11 09:53:36 +0100

    removed tracing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -77,7 +77,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: false, onSameUrlNavigation: 'reload', enableTracing: true}), ToasterModule.forRoot()],
+  imports: [RouterModule.forRoot(routes, { useHash: false, onSameUrlNavigation: 'reload' }), ToasterModule.forRoot()],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
